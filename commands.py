f07e249280cd40478881e5f46d07a046c471971d
Brandisong <brandisong00@gmail.com>
2026-07-04 11:32:48 +1000

    Added echo and daily wisdom (WIP) commands. Tried to test the commands extension but decided to discontinue it. Sticking with the simpler event for now.

---
+++ commands.py
@@ -0,0 +1,5 @@
+# Gets a quote from a list
+from pathlib import Path
+def daily_wisdom():
+    quote_path = Path("./quotes.txt")
+    
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 from os import getenv
 from dotenv import load_dotenv
+import commands
 
 # Load token
 load_dotenv()
@@ -18,15 +19,30 @@ client = discord.Client(intents=intents)
 async def on_ready():
     print(f"Logged in as {client.user}")
 
-# Say hello
+# Screen for commands
 @client.event
 async def on_message(message):
     # Ignore messages from self
     if message.author == client.user:
         return
     
-    if message.content.startswith("$hello"):
+    # List possible commands
+    if message.content.startswith("!help"):
+        await message.channel.send("Possible commands:\n"
+        "!hello\n!echo [message]\n!daily_wisdom")
+    
+    # Say hello
+    if message.content.startswith("!hello"):
         await message.channel.send("Hello!")
+    
+    # Echo
+    if message.content.startswith("!echo"):
+        response: str = message.content[5:]
+        await message.channel.send(response)
+    
+    # Daily wisdom
+    if message.content.startswith("!daily_wisdom"):
+        await message.channel.send(commands.daily_wisdom())
 
 
 # Run the bot
